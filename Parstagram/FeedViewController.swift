a4aeb53b59defb3863d673e80334b560c1a22d94
RODOLFO PUNZALAN <1011904529@wce111-12.local>
2019-03-07 19:02:15 -0800

    adding comments done

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -59,8 +59,25 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.photoView.af_setImage(withURL: url)
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.row]
         
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "Comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
         
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved")
+            } else {
+                print("Error saving comment")
+            }
+        }
     }
     
 
