71f8e0e13916b4f91426eaf75ee248764efc68ec
RODOLFO PUNZALAN <1011904529@wce111-12.local>
2019-03-07 20:50:31 -0800

    added keyboard

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 import Parse
 import AlamofireImage
+import MessageInputBar
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    let commentBar = MessageInputBar()
     
     var posts = [PFObject]()
     
@@ -21,9 +23,22 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        tableView.keyboardDismissMode = .interactive
+        
 
         // Do any additional setup after loading the view.
     }
+    
+    override var inputAccessoryView: UIView? {
+        return commentBar
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    
     override func viewDidAppear(_ animated: Bool) {
     
         super.viewDidAppear(animated)
